1ff1c999f5a5a6d2d393817dee0ab8d231d69f10
Kudriashova Anastasiia <kudriashova.ag@gmail.com>
2024-01-20 17:51:09 +0200

    react start

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ToDoList.css';
+
+const ToDoList = () => {
+
+    let name = 'Tom';
+
+    return (
+        <>
+            <h1 className='primary'>Todo, {name}</h1>
+            <p>hjjghj</p>
+        </>
+    );
+}
+
+
+export default ToDoList;
